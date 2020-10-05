9afc682a0e788778042f97925d1fba4bb462348f
Steven <awi92gemini@gmail.com>
2020-10-05 13:46:42 +0800

    add ScrollToTop

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<Router>
+				<ScrollToTop />
 				<App />
 			</Router>
 		</Provider>
